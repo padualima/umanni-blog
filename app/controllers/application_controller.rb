2fa48bcaa50024b549d61ed02cad9ec34db452dc
Pádua Lima <padualima.dev@gmail.com>
2020-01-01 10:21:13 -0300

    login, registarable and forget password edited

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,24 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if devise_controller? && resource_name == :user
+      "devise"
+    else
+      "application"
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    dashboard_homes_index_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
   def configure_permited_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :image])
   end
