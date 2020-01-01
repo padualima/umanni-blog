e1a6f9dcf411d4be4e91afc90a3a6129c2f27107
Pádua Lima <padualima.dev@gmail.com>
2019-12-31 23:37:06 -0300

    adding the project, with post working

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+User.create!(
+  name: "Pádua Lima",
+  username: "padualima",
+  email: "padua@admin.com",
+  password: 112233,
+  password_confirmation: 112233
+)
+Category.create!(
+  description: "Startup"
+)
