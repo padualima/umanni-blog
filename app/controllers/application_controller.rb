f2c81d12fedc181aec2fa70396c98f03d40c7212
Pádua Lima <padualima.dev@gmail.com>
2019-12-31 23:52:46 -0300

    login add new attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permited_params, if: :devise_controller?
+
+  protected
+
+  def configure_permited_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :image])
+  end
 end
